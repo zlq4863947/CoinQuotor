58f6a4c9336052c152cc2d30c19c98625c059dce
syaku <zlq4863947@gmail.com>
2018-10-01 22:42:56 +0900

    first commit

---
+++ CoinQuotor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CoinQuotor
+//
+//  Created by syaku on 2018/10/01.
+//  Copyright © 2018年 syaku. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CoinQuotor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CoinQuotor
+//
+//  Created by syaku on 2018/10/01.
+//  Copyright © 2018年 syaku. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
